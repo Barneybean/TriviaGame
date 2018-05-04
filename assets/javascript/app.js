1ad13000112c284e757aa3ea0834a56b0c31fbf1
William <William@MacBook-Pro-2.local>
2018-05-03 22:47:51 -0700

    fixed some bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,17 @@ $("#document").ready(function (){
     var qCounter;
     var rightAnswerCounter;
     var wrongAnswerCounter;
+    var unansweredCounter;
     var intervalTime;
     var intervalTrivia
     var isRunning;
+    var temp;
     var isAnswered;
     var button;
-    var setTime=5;
-    var interval=5;
-    var showAnswerTime=2;
+    var choiceDiv;
+    var userChoice;
+    var interval=3;
+    var showAnswerTime=1;
     var choiceArr=[$("#choice1"),$("#choice2"),$("#choice3"),$("#choice4")];
     var game=[
 
@@ -28,94 +31,149 @@ $("#document").ready(function (){
             question:"Who is known as the Turk?",
             choices: ["Sollozzo","Tessio", "McCluskey", "Clemenza"],
             answer:"Sollozzo"
+        },
+        {
+            question:"What business is Don Corleone in? He imports ________.",
+            choices: ["coffee","garments","seafood","olive oil"],
+            answer:"olive oil"
+        },
+        {
+            question:"How is Tom Hagen related to the Family?",
+            choices: ["He is Connie's husband","He is Sonny's cousin","He was adopted","He is Don Corleone's youngest son"],
+            answer: "He was adopted"
         }
-        // {
-        //     question:"",
-        //     choices: [],
-        //     answer:1,
-        // },
-        // {
-        //     question:"",
-        //     choices: [],
-        //     answer:1,
-        // }
     ]
 
     function initialize() {
-        timeCounter=setTime;
+        timeCounter=interval;
         qCounter=0;
         rightAnswerCounter=0;
         wrongAnswerCounter=0;
+        unansweredCounter=0;
         isRunning=false;
         isAnswered=false;
     }
     initialize();
-    console.log(game[qCounter].choices[0]);
+    
     // display button;
     button=$('<button id="startBtn">Start</button>');
     $("#timer").html(button);
-    console.log(button);
-
+   
     $("#startBtn").on("click", function () {
-        startCount();
-        setTimeout(showAnswer,1000*(interval-showAnswerTime));
-        showQuestion();
-        showChoices();
         startTrivia();
-
+        firstQuestion();
+        
+        
     });
 
-    function startTrivia() {
-        intervalTrivia=setInterval(nextQuestion, 1000*interval);
+    $(document).on("click", ".option", function(){
+        userChoice = $(this).attr("data-choice");
+        isAnswered=true;
+        checkifAnswered();
+        // console.log(qCounter === game.length);
+        // console.log(qCounter);
+        // console.log(game.length);
+        if (qCounter === game.length-1) {
+            showSummary();
+            clearInterval(intervalTrivia);
+            isRunning=true;
+            clearInterval(intervalTime);
+        };
+        isAnswered=true;
+    });
+
+    function firstQuestion() {
+        startCount();
+        showQuestion(qCounter);
+        showChoices(qCounter);
+        checkifAnswered();
+    }
+
+    function checkifAnswered() {
+        console.log(isAnswered);
+        if (!isAnswered) {
+            setTimeout(showAnswer,1000*(interval-showAnswerTime));
+            unansweredCounter++;
+            console.log(unansweredCounter);
+        }
+        else {
+            showAnswer();
+        }
         
+    }
+
+    function nextQuestion() {
+        console.log(unansweredCounter);
+        if (unansweredCounter===game.length) {
+            showSummary();
+            clearInterval(intervalTrivia);
+            isRunning=true;
+            clearInterval(intervalTime);
+        }
+        else {
+            qCounter++;
+            showQuestion(); 
+            showChoices();
+            checkifAnswered();
+            $("#notice").empty();
+            $("#answer").empty();
+            console.log(unansweredCounter);
+        }
+    };
+
+    function showQuestion() {
+        $("#question").html(game[qCounter].question);
+        isAnswered=false;
+    };
+    
+    function showChoices() {
+        $("#choices").empty()//*********** !!!!!!!!
+        
+        for (i=0; i<game[qCounter].choices.length;i++) {
+            
+            choiceDiv = $("<button>");
+            var choiceDisplay=game[qCounter].choices[i];
+            choiceDiv.attr("class","option");
+            choiceDiv.attr("data-choice",choiceDisplay);
+            choiceDiv.append(choiceDisplay);
+            $("#choices").append(choiceDiv);   
+        }  
+    };
+   
+    function showAnswer() {
+        stopCount();
+        if(userChoice === game[qCounter].answer) {
+            $("#answer").html("Answer is: " + "<h2>"+game[qCounter].answer+"</h2>" + "You're a True Fan!!" );
+            var rightImage = $("<img>");
+            rightImage.attr("src",'assets/images/right.gif');
+            $("#choices").html(rightImage);
+            rightAnswerCounter++;
+        }
+        else {
+            $("#answer").html("<h2>"+"Hmmmmm, You should watch Godfather Again"+"</h4>")
+            var wrongImage = $("<img>");
+            wrongImage.attr("src","assets/images/wrong.gif");
+            $("#choices").html(wrongImage);
+            wrongAnswerCounter++;
+        }
+    }
+
+    function startTrivia() {
+        intervalTrivia=setInterval(nextQuestion, 1000*(interval));
     };
 
     function stopTrivia() {
         clearInterval(intervalTrivia);
     }
 
-    function nextQuestion() {
-        qCounter++;
-        showQuestion(); 
-        showChoices();
-        setTimeout(showAnswer,1000*(interval-showAnswerTime));
-        
-        if (qCounter === game.length) {
-            showSummary();
-            stopTrivia();
-            isRunning=true;
-        };
-        console.log(game.length);
-    };
-
-    function showQuestion() {
-        console.log(qCounter);
-        $("#question").html(game[qCounter].question);
-    };
-    
-    function showChoices() {
-       
-        $("#choices").html(
-            '<input class="radio" type="radio">'+ " " + game[qCounter].choices[0]+"<br>"+
-            '<input class="radio" type="radio">'+ " " + game[qCounter].choices[1]+"<br>"+
-            '<input class="radio" type="radio">'+ " " + game[qCounter].choices[2]+"<br>"+
-            '<input class="radio" type="radio">'+ " " + game[qCounter].choices[3]+"<br>"
-            
-        );
-        
-        // for (var i=0; i<game[qCounter].choices.length;i++ ) {
-        //     var choices=game[qCounter].choices[i];
-        //     choiceArr[i].html("<button>"+choices+"</button>");
-        // };
-    };
-
-    function showAnswer() {
-        $("#choices").html("Great Job! Answer is: " + game[qCounter].answer);
-    }
-
     
     function showSummary() {
-        
+        stopCount();
+        $("#timmer").empty();
+        $("#choices").empty();
+        $("#answer").empty();
+        $("#notice").empty();
+        $("#choices").html("<h2>"+"Correct: "+ rightAnswerCounter+"</h2>"+"<br>"+"<h2>"+" wrong: "+ wrongAnswerCounter+"</h2>"+"<br>"+"<h2>"+" unanswered: "+ unansweredCounter+"</h2>");
     };
 
     // timer
@@ -131,23 +189,14 @@ $("#document").ready(function (){
         $("#timer").html("Time Remaining: " + timeCounter+"s");
         timeCounter--;
         if (timeCounter === 0) {
-            timeCounter=setTime;
+            timeCounter=interval;
         }
     }
 
-
-
-
-    // function formatChoices() {
-    //     var radio1=$('<input id="choice1" type="radio">game[qCounter].choices[0]');
-    //     var radio2=$('<input id="choice2" type="radio">');
-    //     var radio3=$('<input id="choice3" type="radio">');
-    //     var radio4=$('<input id="choice4" type="radio">');
-    //     $("#choices").append(radio1);
-    //     $("#choices").append(radio2);
-    //     $("#choices").append(radio3);
-    //     $("#choices").append(radio4);
-    //     console.log(radio1);
-    // }
+    function stopCount() {
+        if(isRunning) {
+            clearInterval(intervalTime);
+        }
+    }
 });
 
